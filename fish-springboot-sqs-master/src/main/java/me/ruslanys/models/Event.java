92e21fb0eb6b2e37c259b177a6f1b2c15b1a8da8
RamiBenMohamed <benmohamedrami91@gmail.com>
2017-04-12 13:21:14 +0200

    Update Event.java

--- fish-springboot-sqs-master/src/main/java/me/ruslanys/models/Event.java
+++ fish-springboot-sqs-master/src/main/java/me/ruslanys/models/Event.java
@@ -13,11 +13,7 @@ public class Event {
     private Long taskId;
     private Type type;
     private Status status;
-    public Event(Long taskId,Type type,Status status){
-    	this.taskId= taskId;
-    	this.type=type;
-    	this.status=status;
-    }
+   
     public enum Status {
         STARTED, SUCCESSFUL, FAILED
     }
