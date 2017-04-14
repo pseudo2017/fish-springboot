610a09d84d9dc802f10d8f032604159a0909f083
RamiBenMohamed <benmohamedrami91@gmail.com>
2017-04-14 13:02:23 +0200

    Update Application.java

--- fish-springboot-sqs-master/src/main/java/me/ruslanys/Application.java
+++ fish-springboot-sqs-master/src/main/java/me/ruslanys/Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-	public static final String MANAGER_QUEUE = "mQueue";
-    public static final String PROCESSOR_QUEUE = "cQueue";
+	public static final String MANAGER_QUEUE = "ramiQ";
+    public static final String PROCESSOR_QUEUE = "ramiQa";
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
