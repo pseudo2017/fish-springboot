6d69205a7308796d04933e25f7bbf9626eae1a63
RamiBenMohamed <benmohamedrami91@gmail.com>
2017-04-18 16:36:07 +0200

    Update SchedulerConfig.java

--- fish-springboot-sqs-master/src/main/java/me/ruslanys/configs/SchedulerConfig.java
+++ fish-springboot-sqs-master/src/main/java/me/ruslanys/configs/SchedulerConfig.java
@@ -6,9 +6,7 @@ import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
-/**
- * @author Ruslan Molchanov (ruslanys@gmail.com)
- */
+
 @Configuration
 @EnableScheduling
 public class SchedulerConfig implements SchedulingConfigurer {
